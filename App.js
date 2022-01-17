56e45b38c5ee50e85eb3b7accbc679ad4a1a6caf
Ingrid Lima <ibezerradelima@gmail.com>
2022-01-17 20:51:40 -0300

    Update

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { TouchableOpacity, StyleSheet, TextInput, View, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [url, setUrl] = useState('');
+  const [go, setGo] = useState(false);
+
+  if(go == false){
+    return (
+      <View style={styles.container}>
+          <Text style={styles.title}>Digite a Url</Text> 
+        <TextInput  
+          style={styles.txtInput}
+          onChangeText={text => setUrl(text)}
+          value={url}
+          />
+        <View style={{margin: 5, alignItems: 'center',}}>
+          <TouchableOpacity onPress={() => setGo(true)} style={styles.button}>
+            <Text style={styles.Button}>GO</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }else{
+    return(
+      <>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => setGo(false)} style={styles.btnBack}>
+              <Text style={styles.txtButton}>BACK</Text>
+          </TouchableOpacity>
+      </View>
+        <WebView 
+          source={{ uri: url }}
+          />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#abdf'
+  },
+  title:{
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 50,
+    marginBottom: 20
+  },
+  txtInput:{
+    borderWidth: 4,
+    borderColor:'#ddd',
+    margin: 20,
+    borderRadius: 8,
+    paddingLeft: 10,
+    backgroundColor: '#fff'
+  },
+  button:{
+    width: 90, 
+    height: 55,
     backgroundColor: '#fff',
+    borderColor:'#ddd',
+    borderWidth: 4,
+    borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
+  }, 
+  txtButton:{
+    color: '#111',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
+
+  //Parte 2
+  header:{
+    marginTop: 40,
+    backgroundColor: '#abdf',
+    height: 35,
+    justifyContent: 'center',
+  },
+  btnBack:{
+    marginLeft: 10,
+    paddingLeft: 10,
+    borderLeftWidth: 6
+  }
 });
